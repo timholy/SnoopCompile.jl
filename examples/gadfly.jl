d518551971e5749d260ca3d04a877e74be030fbb
Tim Holy <tim.holy@gmail.com>
2015-09-16 08:53:52 -0500

    Get single-session snooping working, and implement userimg.jl support

--- examples/gadfly.jl
+++ examples/gadfly.jl
@@ -1,24 +1,17 @@
-# Run this after snoop_on.jl
+# After first running snoop_on(), open a new julia session and run this
 
 if !isinteractive()
     error("Because Gadfly uses the display, you must run this interactively")
 end
 
-using SnoopCompile
+using SnoopCompile, Gadfly
 
-using Gadfly, RDatasets, SnoopCompile
-ds1 = dataset("datasets", "iris")
-ds2 = dataset("car", "SLID")
-
-SnoopCompile.@snoop "/tmp/gadfly_compiles.csv" begin
-           display(plot(ds1, x="SepalLength", y="SepalWidth", Geom.point))
-           display(plot(ds2, x="Wages", color="Language", Geom.histogram))
-           display(plot(x=collect(1:100), y=sort(rand(100))))
-           display(plot([sin, cos], 0, 25))
-       end
+SnoopCompile.@snoop1 "/tmp/gadfly_compiles.csv" begin
+    include(Pkg.dir("Gadfly", "test","runtests.jl"))
+end
 
 snoop_off()
 
 data = SnoopCompile.read("/tmp/gadfly_compiles.csv")
 pc, discards = SnoopCompile.parcel(data[end:-1:1,2])
-SnoopCompile.write(pc, "/tmp/precompile")
+SnoopCompile.write("/tmp/precompile", pc)
--- src/SnoopCompile.jl
+++ src/SnoopCompile.jl
@@ -3,7 +3,8 @@ module SnoopCompile
 export
     snoop_on,
     snoop_off,
-    @snoop
+    @snoop,
+    @snoop1
 
 function snoop_path()
     snoopdir = Base.find_in_path("SnoopCompile.jl")
@@ -11,19 +12,94 @@ function snoop_path()
     splitdir(snoopdir)[1]
 end
 
+"""
+`snoop_on()` modifies the julia compiler to turn on logging. To
+actually use logging, you'll need to start a fresh julia session and
+then use `@snoop1`. When finished, restore the compiler to its
+original state with `snoop_off()`.
+"""
 function snoop_on()
     cd(snoop_path()) do
         run(`sh snoop.sh ""`)
     end
 end
 
+"""
+`snoop_off()` restores the julia compiler to its original
+state. Alternatively, you can just use `git checkout src/codegen.cpp
+&& make` from the julia source folder.
+"""
 function snoop_off()
     cd(snoop_path()) do
         run(`sh snoop.sh "--reverse"`)
     end
 end
 
+"""
+```
+@snoop "compiledata.csv" begin
+    # Commands to execute
+end
+```
+causes the julia compiler to log all functions compiled in the course
+of executing the commands to the file "compiledata.csv".  This file
+can be used for the input to `SnoopCompile.read`.
+
+After recompiling `codegen.cpp`, this launches a new julia process in
+which to run the commands. Once finished, it closes the process and
+restores `codegen.cpp` to its original state.  If you're recompiling
+manually with `snoop_on()` and `snoop_off()`, use `@snoop1` in a fresh
+julia session instead.
+"""
 macro snoop(filename, commands)
+    println("Turning on compiler logging")
+    snoop_on()
+    try
+        print("Launching new julia process to run commands...")
+        # addprocs will run the unmodified version of julia, so we
+        # launch it as a command.
+        code_object = """
+            while !eof(STDIN)
+                eval(Main, deserialize(STDIN))
+            end
+            """
+        io, pobj = open(`$(Base.julia_cmd()) --eval $code_object`, "w", STDOUT)
+        serialize(io, quote
+            using SnoopCompile
+        end)
+        # Now that the new process knows about SnoopCompile, it can
+        # deserialize this next expression
+        serialize(io, quote
+            @snoop1 $filename $commands
+        end)
+        close(io)
+        wait(pobj)
+        println("done.")
+    finally
+        println("Restoring compiler to original state")
+        snoop_off()
+    end
+    nothing
+end
+
+"""
+```
+@snoop1 "compiledata.csv" begin
+    # Commands to execute
+end
+```
+causes the julia compiler to log all functions compiled in the course
+of executing the commands to the file "compiledata.csv".  This file
+can be used for the input to `SnoopCompile.read`.
+
+You must run this in a julia session that has been started freshly
+since turning on snooping with `snoop_on()`.
+"""
+macro snoop1(filename, commands)
+    _snoop1(filename, commands)
+end
+
+function _snoop1(filename, commands)
     quote
         io = open($filename, "w")
         ccall(:jl_dump_compiles, Void, (Ptr{Void},), io.handle)
@@ -36,6 +112,9 @@ macro snoop(filename, commands)
     end
 end
 
+"""
+`SnoopCompile.read("compiledata.csv")` reads the log file produced by the compiler and returns the functions as an 2-column array. The first column is the amount of time required to compile each function, the second is the corresponding function + types. The functions are sorted in order of increasing compilation time. (The time does not include the cost of nested compiles.)
+"""
 function read(filename)
     data = readdlm(filename, '\t')
     # Save the most costly for last
@@ -43,48 +122,73 @@ function read(filename)
     data[p,:]
 end
 
-# calls is the second column of the output of read_snoop,
-# after selecting which ones you want to keep.
-function parcel(calls; blacklist=UTF8String[])
+function parse_call(c; subst = Dict(), blacklist=UTF8String[])
+    fpath = [""]
+    args = ""
+    contains(c, "#") && return false, c, fpath, args    # skip gensyms
+    contains(c, "<:") && return false, c, fpath, args   # skip TypeVar types
+    for (k,v) in subst
+        c = replace(c, k, v)
+    end
+    if any(b->contains(c, b), blacklist)
+        println(c, " is blacklisted")
+        return false, c, fpath, args
+    end
+    fargs = split(c, '(')
+    if length(fargs) != 2
+        error("c is ", c, ", fargs is ", fargs)
+    end
+    # Make sure this is a generic function
+    fpath = filter(x->!isempty(x), split(fargs[1], "."))
+    if !isempty(search(c, "anonymous"))
+        return false, c, fpath, args
+    end
+    # Make sure the module is defined
+    local mod
+    try
+        mod = eval(Main, parse(join(fpath[1:end-1], ".")))
+    catch
+        return false, c, fpath, args
+    end
+    if !isdefined(mod, symbol(fpath[end]))
+        println("skipping ", fargs[1])
+        return false, c, fpath, args
+    end
+    fname = convert(UTF8String, fpath[end])
+    chr = first(fname)
+    if !isalnum(chr) && chr!='_'
+        # operators just cause too much trouble to be worth precompiling
+        return false, c, fpath, args
+    end
+    fname = string(mod, ".", fname)
+    args = fargs[2]
+    if length(args) > 1
+        # we insert a comma to ensure tuples for single-argument functions
+        args = string("(", args[1:end-1], ",)")
+    else
+        args = "()"
+    end
+    # Make sure we can eval the arg types
+    try
+        eval(Main, parse(args))
+    catch
+        return false, c, fpath, args
+    end
+    pcstring = string("    precompile(", fname, ", ", args, ")")
+    true, pcstring, fpath, args
+end
+
+"""
+`pc, discards = parcel(calls; subst=Dict(), blacklist = UTF8String[])` assigns each compile statement to the module that owns the function. Perform string substitution via `subst=Dict("Module1"=>"Module2")`, and omit functions in particular modules with `blacklist=["Module3"]`.  On output, `pc["Module2"]` contains all the precompiles assigned to `Module2`, and `discards` contains functions that had arguments spanning modules and which therefore could not be assigned to a particular module.
+
+Use `SnoopCompile.write(prefix, pc)` to generate a series of files in directory `prefix`, one file per module.
+"""
+function parcel(calls; subst=Dict(), blacklist=UTF8String[])
     pc = Dict{UTF8String,Vector{UTF8String}}()
     discards = UTF8String[]
     for c in calls
-        contains(c, "#") && continue
-        contains(c, "<:") && continue
-        if any(b->contains(c, b), blacklist)
-            continue
-        end
-        fargs = split(c, '(')
-        if length(fargs) != 2
-            error("c is ", c, ", fargs is ", fargs)
-        end
-        # Make sure this is a generic function
-        fpath = filter(x->!isempty(x), split(fargs[1], "."))
-        local mod
-        try
-            mod = eval(Main, parse(join(fpath[1:end-1], ".")))
-        catch
-            continue
-        end
-        if !isdefined(mod, symbol(fpath[end]))
-            println("skipping ", fargs[1])
-            continue
-        end
-        fname = convert(UTF8String, fpath[end])
-        chr = first(fname)
-        if !isalnum(chr) && chr!='_'
-            continue   # operators just cause too much trouble...
-            #                fname = string('(', fname, ')')  # + -> (+)
-        end
-        fname = string(mod, ".", fname)
-        args = fargs[2]
-        if length(args) > 1
-            # we insert a comma to ensure tuples for single-argument functions
-            args = string("(", args[1:end-1], ",)")
-        else
-            args = "()"
-        end
-        pcstring = string("    precompile(", fname, ", ", args, ")")
+        keep, pcstring, fpath, args = parse_call(c, blacklist=blacklist)
+        keep || continue
         # Add to the appropriate dictionary
         modname = fpath[1]
         try
@@ -101,8 +205,37 @@ function parcel(calls; blacklist=UTF8String[])
     pc, discards
 end
 
+"""
+`pc = format_userimg(calls; subst=Dict(), blacklist = UTF8String[])` generates precompile directives intended for your base/userimg.jl script.  Use `SnoopCompile.write(filename, pc)` to create a file that you can `include` into `userimg.jl`.
+"""
+function format_userimg(calls; subst=Dict(), blacklist=UTF8String[])
+    pc = Array(UTF8String,0)
+    for c in calls
+        keep, pcstring, fpath, args = parse_call(c, blacklist=blacklist)
+        keep || continue
+        push!(pc, pcstring)
+    end
+    pc
+end
+
+# Write precompiles for userimg.jl
+function write(io::IO, pc::Vector)
+    for ln in pc
+        println(io, ln)
+    end
+end
+function write(filename::AbstractString, pc::Vector)
+    path, fn = splitdir(filename)
+    if !isdir(path)
+        mkpath(path)
+    end
+    open(filename, "w") do io
+        write(io, pc)
+    end
+end
+
 # Write each modules' precompiles to a separate file
-function write(pc, prefix=pwd())
+function write(prefix::AbstractString, pc::Dict)
     if !isdir(prefix)
         mkpath(prefix)
     end
