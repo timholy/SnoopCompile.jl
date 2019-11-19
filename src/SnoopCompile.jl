adef2720d6436e0ad2d71531f32d134f21b689f8
Tim Holy <tim.holy@gmail.com>
2019-11-19 13:20:44 -0600

    Tweak `anonrex` (#31)

--- src/SnoopCompile.jl
+++ src/SnoopCompile.jl
@@ -171,7 +171,7 @@ function extract_topmod(e)
     return :unknown
 end
 
-const anonrex = r"##\d*#{1,2}\d*"   # detect anonymous functions
+const anonrex = r"#{1,2}\d+#{1,2}\d+"   # detect anonymous functions
 
 function parse_call(line; subst=Vector{Pair{String, String}}(), blacklist=String[])
     match(anonrex, line) === nothing || return false, line, :unknown, ""
