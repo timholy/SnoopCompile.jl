4fe0bf3d3428bbe822d212e5d8ff9d9e0216d1b0
aminya <aminyahyaabadi74@gmail.com>
2019-12-29 02:31:28 +0330

    snoopiBot for runtests

--- src/utilities.jl
+++ src/utilities.jl
@@ -154,10 +154,34 @@ macro snoopiBot(packageName::String, snoopScript::Expr)
 
 end
 
+################################################################
+using Pkg
+
+"""
+Should be removed once Pkg allows adding test dependencies to the current environment
+
+Used in workflow file
+"""
+function addtestdep()
+    local testToml
+    try
+        testToml = Pkg.Types.parse_toml("test/Test.toml")
+    catch
+        error("please add a Test.toml to the /test directory for test dependencies")
+    end
+
+    for (name, uuid) in testToml["deps"]
+        Pkg.add(Pkg.PackageSpec(name = name, uuid = uuid))
+    end
+end
+################################################################
+
 macro snoopiBot(packageName::String)
     package = Symbol(packageName)
     snoopScript = :(
-        using package, Pkg; Pkg.test(packageName)
+        using $(package);
+        runtestpath = joinpath(dirname(dirname(pathof($(package)))), "test", "runtests.jl");
+        include(runtestpath);
     )
     return quote
         @snoopiBot $packageName $(esc(snoopScript))
