c27fc058074a8a079823fee543d48e20e6c1ef4f
aminya <aminyahyaabadi74@gmail.com>
2019-12-31 05:46:32 +0330

    Adding BotConfig object
    Allows feeding more options to the macro without changing the API

--- src/bot.jl
+++ src/bot.jl
@@ -1,4 +1,4 @@
-export precompileActivator, precompileDeactivator, precompilePather, @snoopiBot, @snoopiBenchBot
+export precompileActivator, precompileDeactivator, precompilePather, @snoopiBot, @snoopiBenchBot, BotConfig
 
 """
     precompilePather(packageName::String)
@@ -121,7 +121,20 @@ function precompileDeactivator(packagePath::String, precompilePath::String)
 end
 ################################################################
 """
-    snoopiBot(packageName::String, snoopScript)
+    BotConfig
+
+Config object that holds the options and configuration for the snoopCompile bot. This object is fed to the `@snoopiBot`.
+"""
+struct BotConfig
+    packageName::String
+    blacklist::Vector{Union{AbstractString,Regex,AbstractChar}}
+end
+
+BotConfig(packageName::String) = BotConfig(packageName, String[])
+
+################################################################
+"""
+    @snoopiBot config::BotConfig snoopScript
 
 macro that generates precompile files and includes them in the package. Calls other bot functions.
 
@@ -140,17 +153,11 @@ using SnoopCompile
   include(joinpath(examplePath,"Language_Fundamentals", "Data_Types", "usage_Numeric_Types.jl"))
 end
 ```
-
-If you do not have additional examples, you can use your runtests.jl file. To do that use:
-
-```julia
-using SnoopCompile
-
-# using runtests:
-@snoopiBot "MatLang"
-```
 """
-macro snoopiBot(packageName::String, snoopScript::Expr, blacklist::Vector{Union{AbstractString,Regex,AbstractChar}}=String[])
+macro snoopiBot(config::BotConfig, snoopScript)
+
+    packageName = config.packageName
+    blacklist= config.blacklist
     ################################################################
     packagePath = joinpath(pwd(),"src","$packageName.jl")
     precompilePath, precompileFolder = precompilePather(packageName)
@@ -170,7 +177,7 @@ macro snoopiBot(packageName::String, snoopScript::Expr, blacklist::Vector{Union{
 
         ################################################################
         ### Parse the compiles and generate precompilation scripts
-        pc = SnoopCompile.parcel(data, blacklist = blacklist)
+        pc = SnoopCompile.parcel(data, blacklist = $blacklist)
         onlypackage = Dict( packageSym => sort(pc[packageSym]) )
         SnoopCompile.write($precompileFolder,onlypackage)
         ################################################################
@@ -179,15 +186,57 @@ macro snoopiBot(packageName::String, snoopScript::Expr, blacklist::Vector{Union{
 
 end
 
-macro snoopiBot(packageName::String, blacklist::Vector{Union{AbstractString,Regex,AbstractChar}}=String[])
-    package = Symbol(packageName)
-    snoopScript = :(
-        using $(package);
-        runtestpath = joinpath(dirname(dirname(pathof($(package)))), "test", "runtests.jl");
-        include(runtestpath);
-    )
+macro snoopiBot(packageName::String, snoopScript)
+    config = BotConfig(packageName)
     return quote
-        @snoopiBot $packageName $(esc(snoopScript)) blacklist
+        @snoopiBot $config $(esc(snoopScript))
+    end
+end
+macro snoopiBot(configExpr, snoopScript)
+    config = eval(configExpr)
+    return quote
+        @snoopiBot $config $(esc(snoopScript))
+    end
+end
+
+"""
+    @snoopiBot config::BotConfig
+
+If you do not have additional examples, you can use your runtests.jl file. To do that use:
+
+```julia
+using SnoopCompile
+
+# using runtests:
+@snoopiBot "MatLang"
+```
+"""
+macro snoopiBot(config::BotConfig)
+
+    packageName = config.packageName
+
+    package = Symbol(packageName)
+    snoopScript = esc(quote
+        using $(package)
+        runtestpath = joinpath(dirname(dirname(pathof( $package ))), "test", "runtests.jl")
+        include(runtestpath)
+    end)
+    return quote
+        @snoopiBot $config $(esc(snoopScript))
+    end
+end
+
+macro snoopiBot(packageName::String)
+    config = BotConfig(packageName)
+    return quote
+        @snoopiBot $config
+    end
+end
+
+macro snoopiBot(configExpr)
+    config = eval(configExpr)
+    return quote
+        @snoopiBot $config
     end
 end
 
