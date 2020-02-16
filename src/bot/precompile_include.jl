4724182f1ef6b04959d9743ca36fd56332716af6
aminya <aminyahyaabadi74@gmail.com>
2020-02-16 03:26:43 -0600

    precompile_pather

--- src/bot/precompile_include.jl
+++ src/bot/precompile_include.jl
@@ -21,3 +21,28 @@ allos = [Sys.iswindows,
     end
     return output
 end
+################################################################
+"""
+    precompile_pather(package_name::String)
+
+To get the path of precompile_package_name.jl file
+
+Written exclusively for SnoopCompile Github actions.
+# Examples
+```julia
+precompile_path, precompileFolder = precompile_pather("MatLang")
+```
+"""
+function precompile_pather(package_name::String, ismultios::Bool)
+    if !ismultios
+        return "\"../deps/SnoopCompile/precompile/precompile_$package_name.jl\"",
+        "$(pwd())/deps/SnoopCompile/precompile/"
+    else
+        os = detectos()
+        return "\"../deps/SnoopCompile/precompile/$os/precompile_$package_name.jl\"",
+        "$(pwd())/deps/SnoopCompile/precompile/$os"
+    end
+end
+
+precompile_pather(package_name::Symbol, ismultios::Bool) = precompile_pather(string(package_name))
+precompile_pather(package_name::Module, ismultios::Bool) = precompile_pather(string(package_name))
