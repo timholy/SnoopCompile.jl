9218493eeabe737b960db8290a20465fe69387bd
aminya <aminyahyaabadi74@gmail.com>
2020-02-16 03:27:00 -0600

    add_includer

--- src/bot/precompile_include.jl
+++ src/bot/precompile_include.jl
@@ -73,3 +73,72 @@ function create_includer_file(package_path::String, precompile_path::String)
     end
 end
 ################################################################
+"""
+    add_includer(package_path::String, precompile_path::String)
+
+Writes the `include(precompile_includer.jl)` to the package file.
+"""
+function add_includer(package_path::String, precompile_path::String)
+    if !isfile(package_path)
+        error("$package_path file doesn't exist")
+    end
+
+    # read package
+    package_text = Base.read(package_path, String)
+
+    # Checks if any other precompile code already exists
+    if occursin("_precompile_()",package_text)
+        error("""Please remove `_precompile_()` and any other code that includes a `_precompile_()` function from $package_path
+        New version of SnoopCompile automatically creates the code.
+        """)
+    elseif occursin(r"#\s*include\(\"precompile_includer.jl\"\)", package_text)
+        error("""Please uncomment `\"include(\"precompile_includer.jl\")\"`
+        Set `should_precompile = false` instead for disabling precompilation.
+        """)
+    end
+
+    # Adding include to source
+    if occursin("include(\"precompile_includer.jl\")", package_text)
+        # has precompile_includer
+        @info "Package already has \"include(\"precompile_includer.jl\")\""
+        return nothing
+    else
+        # no precompile_includer
+        @info "SnoopCompile will try to write  \"include(\"precompile_includer.jl\")\" before end of the module in $package_path. Assume that the last `end` is the end of a module."
+
+        # open lines
+        package_lines = Base.open(package_path) do io
+            Base.readlines(io, keep=true)
+        end
+
+        ## find end of a module
+        # assumes that the last `end` is the end of a module
+        endline = length(package_lines)
+        for iLine = endline:1
+            if any(occursin.(["end # module", "end"], Ref(package_lines[iLine])))
+                endline = iLine
+                break
+            end
+        end
+
+        # add line or error
+        try
+            code = """
+            "include("precompile_includer.jl")"
+            """
+            insert!(lines,iLine-1,code) # add new empty line before the end
+        catch e
+            @error("Error occured during writing", e)
+            return nothing
+        end
+
+        # write the lines
+        if lines != nothing
+            open(package_path, "w") do io
+                for l in lines
+                    write(io, l)
+                end
+            end
+        end
+    end
+end
