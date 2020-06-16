86ae02fdfc04d3015b4734973cc4302c2730d57c
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-06-16 16:40:20 -0500

    SnoopCompileCore

---
+++ src/SnoopCompileCore/SnoopCopmileCore.jl
@@ -0,0 +1,19 @@
+module SnoopCompileCore
+
+using Core: MethodInstance, CodeInfo
+
+# @snoopi and @snoopc are exported from their files of definition
+
+
+include("snoopc.jl")
+
+if VERSION >= v"1.2.0-DEV.573"
+    include("snoopi.jl")
+end
+
+
+if VERSION >= v"1.6.0-DEV.154"
+    include("invalidations.jl")
+end
+
+end
