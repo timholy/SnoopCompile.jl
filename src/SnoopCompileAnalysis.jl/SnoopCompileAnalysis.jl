efaf5154bba505b5f114aeea021cf84b791b96c6
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-06-16 16:40:46 -0500

    SnoopCompileAnalysis

---
+++ src/SnoopCompileAnalysis.jl/SnoopCompileAnalysis.jl
@@ -0,0 +1,24 @@
+module SnoopCompileAnalysis
+
+using Core: MethodInstance, CodeInfo
+using Serialization, OrderedCollections
+
+# Parcel Regex
+const anonrex = r"#{1,2}\d+#{1,2}\d+"         # detect anonymous functions
+const kwrex = r"^#kw##(.*)$|^#([^#]*)##kw$"   # detect keyword-supplying functions
+const kwbodyrex = r"^##(\w[^#]*)#\d+"         # detect keyword body methods
+const genrex = r"^##s\d+#\d+$"                # detect generators for @generated functions
+const innerrex = r"^#[^#]+#\d+"               # detect inner functions
+
+# Parcel
+include("parcel_snoopc.jl")
+
+if VERSION >= v"1.2.0-DEV.573"
+    include("parcel_snoopi.jl")
+end
+
+# Write
+include("write.jl")
+
+
+end # module
