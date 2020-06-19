194965c532e2d4e8ec0e029a6526edaa0fda09e3
Tim Holy <tim.holy@gmail.com>
2020-06-19 12:13:06 -0500

    Prepare the release

--- deps/build.jl
+++
@@ -1,6 +0,0 @@
-using Pkg
-rootdir = dirname(@__DIR__)
-Pkg.activate(rootdir)
-Pkg.develop(PackageSpec(path=joinpath(rootdir,"SnoopCompileCore")))
-Pkg.develop(PackageSpec(path=joinpath(rootdir,"SnoopCompileAnalysis")))
-Pkg.develop(PackageSpec(path=joinpath(rootdir,"SnoopCompileBot")))
