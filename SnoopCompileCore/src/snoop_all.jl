24c8c475f480fdb68d86fec0780205c8a18f0f00
Nathan Daly <nhdaly@gmail.com>
2021-07-26 11:48:22 -0400

    Add docstring to snoop_all

--- SnoopCompileCore/src/snoop_all.jl
+++ SnoopCompileCore/src/snoop_all.jl
@@ -1,17 +1,31 @@
 """
+```
+SnoopCompileCore.@snoop_all "output_filename" begin
+    # Commands to execute
+end
+```
+
+Runs provided commands *in the current process*, and records snoop compilation results for
+all phases of the compiler supported by SnoopCompile: Inference, LLVM Optimization, Codegen.
+
+Returns a tuple of four values:
+- `tinf`: Results from `@snoopi_deep`
+- `"snoopl.csv"`: File 1 of results for `@snoopl`
+- `"snoopl.yaml"`: File 2 of results for `@snoopl`
+- `"snoopc.csv"`: Results file for `@snoopc`
 """
 macro snoop_all(fname_prefix, commands)
     snoopl_csv_f, snoopl_yaml_f, snoopc_csv_f =
         "$fname_prefix.snoopl.csv", "$fname_prefix.snoopl.yaml", "$fname_prefix.snoopc.csv"
-    outsym = gensym(:output)
+    tinf = gensym(:tinf)
     esc(quote
         @snoopc pspawn=false $snoopc_csv_f begin
             @snoopl pspawn=false $snoopl_csv_f $snoopl_yaml_f begin
-                global $outsym = @snoopi_deep begin
+                global $tinf = @snoopi_deep begin
                     @eval $commands
                 end
             end
         end;
-        $outsym, $snoopl_csv_f, $snoopl_yaml_f, $snoopc_csv_f
+        $tinf, $snoopl_csv_f, $snoopl_yaml_f, $snoopc_csv_f
     end)
 end
