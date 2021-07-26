c06d84e0fcc245c1c539cff0ef74cfed7d1f3bc8
Nathan Daly <nhdaly@gmail.com>
2021-07-26 19:21:12 -0400

    Add documentation links & example to docstring

--- SnoopCompileCore/src/snoop_all.jl
+++ SnoopCompileCore/src/snoop_all.jl
@@ -9,10 +9,15 @@ Runs provided commands *in the current process*, and records snoop compilation r
 all phases of the compiler supported by SnoopCompile: Inference, LLVM Optimization, Codegen.
 
 Returns a tuple of four values:
-- `tinf`: Results from `@snoopi_deep`
-- `"snoopl.csv"`: File 1 of results for `@snoopl`
-- `"snoopl.yaml"`: File 2 of results for `@snoopl`
-- `"snoopc.csv"`: Results file for `@snoopc`
+- `tinf`: Results from `@snoopi_deep` - See https://timholy.github.io/SnoopCompile.jl/stable/snoopi_deep/#Viewing-the-results
+- `"snoopl.csv"`: File 1 of results for `@snoopl` - See https://timholy.github.io/SnoopCompile.jl/stable/reference/#SnoopCompile.read_snoopl
+- `"snoopl.yaml"`: File 2 of results for `@snoopl` - See https://timholy.github.io/SnoopCompile.jl/stable/reference/#SnoopCompile.read_snoopl
+- `"snoopc.csv"`: Results file for `@snoopc` - See https://timholy.github.io/SnoopCompile.jl/stable/snoopc/
+
+# Example
+```julia
+julia> tinf, snoopl_csv, snoopl_yaml, snoopc_csv = @snoop_all "foo" foo(1, 2, 3);
+```
 """
 macro snoop_all(fname_prefix, commands)
     snoopl_csv_f, snoopl_yaml_f, snoopc_csv_f =
