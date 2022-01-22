64a368457e5a8aa6e725bd0b56dd6a0309be5039
Tim Holy <tim.holy@gmail.com>
2022-01-22 06:42:07 -0600

    Detect stray clicks in pgdsgui
    Fixes #269

--- src/visualizations.jl
+++ src/visualizations.jl
@@ -26,15 +26,18 @@ that method.
     `pgdsgui` depends on PyPlot via the Requires.jl package. You must load both SnoopCompile and PyPlot for this function to be defined.
 """
 function pgdsgui(ax::PyCall.PyObject, ridata::AbstractVector{Pair{Union{Method,MethodLoc},PGDSData}}; bystr, consts, markersz=25, linewidth=0.5, t0 = 0.001, interactive::Bool=true, kwargs...)
-    methodref = Ref{Union{Method,MethodLoc}}()
+    methodref = Ref{Union{Method,MethodLoc}}()   # returned to the user for inspection of clicked methods
     function onclick(event)
         xc, yc = event.xdata, event.ydata
+        (xc === nothing || yc === nothing) && return
+        # Find dot closest to the click position
         idx = argmin((log.(rts .+ t0) .- log(xc)).^2 + (log.(its .+ t0) .- log(yc)).^2)
         m = meths[idx]
-        methodref[] = m
+        methodref[] = m      # store the clicked method
         println(m, " ($(nspecs[idx]) specializations)")
     end
 
+    # Unpack the inputs into a form suitable for plotting
     meths, rts, its, nspecs, ecols = Union{Method,MethodLoc}[], Float64[], Float64[], Int[], Tuple{Float64,Float64,Float64}[]
     for (m, d) in ridata  # (rt, trtd, it, nspec)
         push!(meths, m)
@@ -46,6 +49,7 @@ function pgdsgui(ax::PyCall.PyObject, ridata::AbstractVector{Pair{Union{Method,M
     sp = sortperm(nspecs)
     meths, rts, its, nspecs, ecols = meths[sp], rts[sp], its[sp], nspecs[sp], ecols[sp]
 
+    # Plot
     # Add t0 to each entry to handle times of zero in the log-log plot
     smap = ax.scatter(rts .+ t0, its .+ t0, markersz, nspecs; norm=plt.matplotlib.colors.LogNorm(), edgecolors=ecols, linewidths=linewidth, kwargs...)
     ax.set_xscale("log")
