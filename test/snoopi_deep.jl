29e9c32afa2ea4b0bb53518d310aacde942065c8
Nathan Daly <nhdaly@gmail.com>
2022-10-20 22:40:07 -0600

    Reorder tests: keep JET tests last

--- test/snoopi_deep.jl
+++ test/snoopi_deep.jl
@@ -945,21 +945,6 @@ end
     Pkg.activate(cproj)
 end
 
-if Base.VERSION >= v"1.7"
-    @testset "JET integration" begin
-        f(c) = sum(c[1])
-        c = Any[Any[1,2,3]]
-        tinf = @snoopi_deep f(c)
-        rpt = SnoopCompile.JET.@report_call f(c)
-        @test isempty(SnoopCompile.JET.get_reports(rpt))
-        itrigs = inference_triggers(tinf)
-        irpts = report_callees(itrigs)
-        @test only(irpts).first == last(itrigs)
-        @test !isempty(SnoopCompile.JET.get_reports(only(irpts).second))
-        @test  isempty(SnoopCompile.JET.get_reports(report_caller(itrigs[end])))
-    end
-end
-
 _name(frame::SnoopCompileCore.InferenceTiming) = frame.mi_info.mi.def.name
 
 @testset "reentrant concurrent profiles 1 - overlap" begin
@@ -1219,4 +1204,19 @@ end
     # Test Cleanup
     @test isempty(SnoopCompileCore.SnoopiDeepParallelism.invocations)
     @test isempty(Core.Compiler.Timings._timings[1].children)
+end
+
+if Base.VERSION >= v"1.7"
+    @testset "JET integration" begin
+        f(c) = sum(c[1])
+        c = Any[Any[1,2,3]]
+        tinf = @snoopi_deep f(c)
+        rpt = SnoopCompile.JET.@report_call f(c)
+        @test isempty(SnoopCompile.JET.get_reports(rpt))
+        itrigs = inference_triggers(tinf)
+        irpts = report_callees(itrigs)
+        @test only(irpts).first == last(itrigs)
+        @test !isempty(SnoopCompile.JET.get_reports(only(irpts).second))
+        @test  isempty(SnoopCompile.JET.get_reports(report_caller(itrigs[end])))
+    end
 end
