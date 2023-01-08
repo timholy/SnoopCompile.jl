f76631cc87331d30c60b67f0b0e12ef5c39b51d1
t-bltg <tf.bltg@gmail.com>
2023-01-08 12:44:00 +0100

    change to `skip_precompile` list

--- SnoopPrecompile/src/SnoopPrecompile.jl
+++ SnoopPrecompile/src/SnoopPrecompile.jl
@@ -2,16 +2,19 @@ module SnoopPrecompile
 
 export @precompile_all_calls, @precompile_setup
 
+@static if VERSION >= v"1.6"
+    using Preferences
+end
+
+@static if VERSION >= v"1.6"
+    const skip_precompile = @load_preference("skip_precompile", String[])
+else
+    const skip_precompile = String[]
+end
+
 const verbose = Ref(false)    # if true, prints all the precompiles
 const have_inference_tracking = isdefined(Core.Compiler, :__set_measure_typeinf)
 const have_force_compile = isdefined(Base, :Experimental) && isdefined(Base.Experimental, Symbol("#@force_compile"))
-
-@static if Base.VERSION >= v"1.6"
-    using Preferences
-    const do_precompile = @load_preference("precompile", true)
-else
-    const do_precompile = true
-end
 
 function precompile_roots(roots)
     @assert have_inference_tracking
@@ -51,7 +54,7 @@ end
         - indirect runtime-dispatched calls to such methods.
 """
 macro precompile_all_calls(ex::Expr)
-    do_precompile || return :()
+    string(__module__) in skip_precompile && return :()
     if have_force_compile
         ex = quote
             begin
@@ -109,7 +112,7 @@ runtime dispatches (though they will be precompiled anyway if the runtime-callee
 to your package).
 """
 macro precompile_setup(ex::Expr)
-    do_precompile || return :()
+    string(__module__) in skip_precompile && return :()
     return esc(quote
         # let
             if ccall(:jl_generating_output, Cint, ()) == 1 || $SnoopPrecompile.verbose[]
