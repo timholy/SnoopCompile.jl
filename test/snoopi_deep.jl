c71b851cb8a11125afbb15aa6b02ead23317d410
Tim Holy <tim.holy@gmail.com>
2023-03-18 02:35:38 -0500

    Vendor `sum` for JET test

--- test/snoopi_deep.jl
+++ test/snoopi_deep.jl
@@ -958,10 +958,19 @@ end
 
 if Base.VERSION >= v"1.7"
     @testset "JET integration" begin
-        f(c) = sum(c[1])
-        c = Any[Any[1,2,3]]
-        tinf = @snoopi_deep f(c)
-        rpt = SnoopCompile.JET.@report_call f(c)
+        function mysum(c)   # vendor a simple version of `sum`
+            isempty(c) && return zero(eltype(c))
+            s = first(c)
+            for x in Iterators.drop(c, 1)
+                s += x
+            end
+            return s
+        end
+        call_mysum(cc) = mysum(cc[1])
+
+        cc = Any[Any[1,2,3]]
+        tinf = @snoopi_deep call_mysum(cc)
+        rpt = SnoopCompile.JET.@report_call call_mysum(cc)
         @test isempty(SnoopCompile.JET.get_reports(rpt))
         itrigs = inference_triggers(tinf)
         irpts = report_callees(itrigs)
