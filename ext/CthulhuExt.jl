34b419cdaaee7b173d6fe3665b16f459ab4407e9
Tim Holy <tim.holy@gmail.com>
2024-07-23 07:26:05 -0500

    Fix Cthulhu extension (#390)

--- ext/CthulhuExt.jl
+++ ext/CthulhuExt.jl
@@ -1,7 +1,7 @@
 module CthulhuExt
     import Cthulhu
     using Core: MethodInstance
-    using SnoopCompile: InstanceNode, TriggerNode, Suggested, InferenceTrigger, countchildren
+    using SnoopCompile: InstanceNode, TriggerNode, Suggested, InferenceTrigger, countchildren, callingframe, callerinstance
 
 
     # Originally from invalidations.jl
@@ -12,13 +12,13 @@ module CthulhuExt
 
     # Originally from parcel_snoop_inference.jl
 
-    Cthulhu.descend(itrig::InferenceTrigger; kwargs...) = descend(callerinstance(itrig); kwargs...)
+    Cthulhu.descend(itrig::InferenceTrigger; kwargs...) = Cthulhu.descend(callerinstance(itrig); kwargs...)
     Cthulhu.instance(itrig::InferenceTrigger) = MethodInstance(itrig.node)
     Cthulhu.method(itrig::InferenceTrigger) = Method(itrig.node)
     Cthulhu.specTypes(itrig::InferenceTrigger) = Cthulhu.specTypes(Cthulhu.instance(itrig))
     Cthulhu.backedges(itrig::InferenceTrigger) = (itrig.callerframes,)
     Cthulhu.nextnode(itrig::InferenceTrigger, edge) = (ret = callingframe(itrig); return isempty(ret.callerframes) ? nothing : ret)
 
-    Cthulhu.ascend(node::TriggerNode) = ascend(node.itrig)
-    Cthulhu.ascend(s::Suggested) = ascend(s.itrig)
+    Cthulhu.ascend(node::TriggerNode) = Cthulhu.ascend(node.itrig)
+    Cthulhu.ascend(s::Suggested) = Cthulhu.ascend(s.itrig)
 end
