ff233b3268d3e7656de8f516f51a335de9821b58
Tim Holy <tim.holy@gmail.com>
2024-07-27 19:40:42 -0500

    Test Cthulhu extension (#392)
    `ascend` is a terminal program so it takes a bit of effort to test it.
    
    xref https://github.com/JuliaDebug/Cthulhu.jl/issues/581

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,14 @@
 using Test
 using SnoopCompile
 
-include("snoop_inference.jl")
-include("snoop_llvm.jl")
-include("snoop_invalidations.jl")
+if !isempty(ARGS)
+    "cthulhu" ∈ ARGS && include("extensions/cthulhu.jl")
+else
+    include("snoop_inference.jl")
+    include("snoop_llvm.jl")
+    include("snoop_invalidations.jl")
 
-# otherwise-untested demos
-retflat = SnoopCompile.flatten_demo()
-@test !isempty(retflat.children)
+    # otherwise-untested demos
+    retflat = SnoopCompile.flatten_demo()
+    @test !isempty(retflat.children)
+end
