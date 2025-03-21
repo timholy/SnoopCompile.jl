5d48a06ae1d358f2b4cf17d5c39f09d183ed0b73
Tim Holy <tim.holy@gmail.com>
2025-03-21 11:24:30 -0500

    Fix test failures on 1.11 (#413)
    Mostly, this compensates for a bug present in Julia 1.11.3 and 1.11.4.
    xref https://github.com/timholy/SnoopCompile.jl/pull/413#issuecomment-2743794491

--- test/testmodules/Stale/StaleC/src/StaleC.jl
+++ test/testmodules/Stale/StaleC/src/StaleC.jl
@@ -2,9 +2,9 @@ module StaleC
 
 using StaleA
 
-StaleA.stale(x::String) = length(x)
-call_buildstale(x) = StaleA.build_stale(x)
+StaleA.stale(x::String) = length(x)         # piracy is great (but not required) for triggering invalidations
+call_buildstale(x) = StaleA.build_stale(x)  # linking build_stale to a method in this module ensures caching
 
-call_buildstale("hey")
+call_buildstale("hey")                      # forces re/precompilation of `StaleA.use_stale(::Vector{Any})`
 
 end # module
