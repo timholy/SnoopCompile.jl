43acfc9d4c9b4a3d618e525f7c0d3df45ec7b31a
Tim Holy <tim.holy@gmail.com>
2025-08-29 07:37:09 -0500

    Split out JET test, make optional

--- test/snoop_inference.jl
+++ test/snoop_inference.jl
@@ -941,27 +941,3 @@ end
 
     Pkg.activate(cproj)
 end
-
-using JET, Cthulhu
-
-@testset "JET integration" begin
-    function mysum(c)   # vendor a simple version of `sum`
-        isempty(c) && return zero(eltype(c))
-        s = first(c)
-        for x in Iterators.drop(c, 1)
-            s += x
-        end
-        return s
-    end
-    call_mysum(cc) = mysum(cc[1])
-
-    cc = Any[Any[1,2,3]]
-    tinf = @snoop_inference call_mysum(cc)
-    rpt = @report_call call_mysum(cc)
-    @test isempty(JET.get_reports(rpt))
-    itrigs = inference_triggers(tinf)
-    irpts = report_callees(itrigs)
-    @test only(irpts).first == last(itrigs)
-    @test !isempty(JET.get_reports(only(irpts).second))
-    @test  isempty(JET.get_reports(report_caller(itrigs[end])))
-end
